0df415ae47de7c8a099888f277574aaddc69c7aa
Christopher Bonilla <Bullquenepas>
2022-05-04 19:15:37 -0400

    Registered admin x2 + views re work.

--- backend/replies/views.py
+++ backend/replies/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAuthenticatedOrReadOnly
+from rest_framework.decorators import api_view, permission_classes
+from .models import Reply
+from comments.models import Comment
+from .serializers import ReplySerializer
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+@api_view(['GET','POST'])
+@permission_classes([IsAuthenticated])
+def reply_list(request):
+    comment_id = request.query_params.get('commentId')
+    comment_instance = Comment.objects.get(id=comment_id)
+
+    if request.method == 'GET':
+        queryset = Reply.objects.filter(comment = comment_id)
+        serializer = ReplySerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    if request.method == 'POST':
+        serializer = ReplySerializer(data=request.data, partial=True)
+        comment_id = int(comment_id)
+        if serializer.is_valid():
+            serializer.save(comment=comment_instance, user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
